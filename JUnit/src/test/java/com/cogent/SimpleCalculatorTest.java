bb650331789df8a403efadf3803e0fbad9a5e427
Greg Newby <arinewby@ymail.com>
2023-03-02 08:44:33 -0600

    comments

--- JUnit/src/test/java/com/cogent/SimpleCalculatorTest.java
+++ JUnit/src/test/java/com/cogent/SimpleCalculatorTest.java
@@ -5,10 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class SimpleCalculatorTest {
+	
+	/*
+	 * Each test method should do the best to test full coverage of the class in test.
+	 * You want to set each assert in it's own test method annotated with @test.
+	 * That way you can find out which tests fail and why.
+	 * It is also a good idea to name the test methods specifically such as twoPlusTwoShouldBeFour.
+	*/
 
 	//2 plus 2 should equal 4
 	@Test
-	void testAdd() {
+	void twoPlusTwoShouldBeFour() {
 		SimpleCalculator sc = new SimpleCalculator();
 		assertEquals(4, sc.add(2, 2));
 	}
